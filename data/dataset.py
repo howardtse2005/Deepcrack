d35a06659e66aebba63d4095833da9e9c75a9a0a
henryshum0 <henryshum0@gmail.com>
2025-07-22 16:03:44 +0800

    changed: dataset class added: random jittering

--- data/dataset.py
+++ data/dataset.py
@@ -1,33 +1,107 @@
 import preprocess_pipeline as pp
 from torch.utils.data import Dataset
-from config import Config as cfg
-
-
-
+from os.path import exists, join
+import os, cv2
+import numpy as np
 class loadedDataset(Dataset):
     """
     Create a torch Dataset from data
     """
 
-    def __init__(self, dataset, preprocess=None):
-        super(loadedDataset, self).__init__()
-        self.samples = []
-        if preprocess is None:
-            preprocess = lambda x: x
+    def __init__(self, dataset_img_path:str, dataset_mask_path:str, augmentations:list[pp.Augmentation]=[],
+                 mask_postfix:str='_GT.png'):
+        super().__init__()
+        self.pp = pp.PreprocessPipeline(augmentations) 
+        if not exists(dataset_img_path):
+            raise FileNotFoundError(f"Dataset path {dataset_img_path} does not exist.")
+        if not exists(dataset_mask_path):
+            raise FileNotFoundError(f"Dataset path {dataset_mask_path} does not exist.")
         
-        # Flatten all crops from all images
-        print("Preprocessing dataset...")
-        for item in dataset:
-            crops = preprocess(item)
-            if isinstance(crops, list):
-                self.samples.extend(crops)
-            else:
-                self.samples.append(crops)
-        print(f"Total samples after processing: {len(self.samples)}")
-
-    def __getitem__(self, index):
-        return self.samples[index]
+        image_files, mask_files = self._get_files(dataset_img_path, dataset_mask_path)
+        imgs, masks = self._get_images(image_files, mask_files, dataset_img_path, dataset_mask_path, mask_postfix)
+        self.imgs, self.masks = self.pp(imgs, masks) # apply augmentations
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.imgs)
+    
+    def __getitem__(self, index):
+        if index < 0 or index >= len(self.imgs):
+            raise IndexError("Index out of range.")
+        img, mask = self.imgs[index], self.masks[index]
+        return img, mask
 
+    def _get_files(self, img_dir, mask_dir):
+        '''
+        Get all image and mask files in the specified directories. and check validity
+        '''
+        image_files = [f for f in os.listdir(img_dir) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+        mask_files = [f for f in os.listdir(mask_dir) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+        if not image_files:
+            raise FileExistsError(f"No image files found in {img_dir}")
+        if not mask_files:
+            raise FileExistsError(f"No mask files found in {mask_dir}")
+        if len(image_files) != len(mask_files):
+            raise ValueError("Number of images and masks do not match.")
+        return image_files, mask_files
+    
+    def _get_images(self, image_files, mask_files, dataset_img_path, dataset_mask_path, mask_postfix):
+        imgs, masks = [], []
+        for img_file in image_files:
+            base_name = os.path.splitext(img_file)[0]
+            mask_file = f"{base_name}{mask_postfix}"
+            if mask_file in mask_files:
+                img_path = join(dataset_img_path, img_file)
+                mask_path = join(dataset_mask_path, mask_file)
+                img, mask = self._read_image(img_path, mask_path)
+                imgs.append(img)
+                masks.append(mask)
+            else:
+                raise FileNotFoundError(f"Mask file {mask_file} not found for image {img_file}.")
+        return imgs, masks
+    
+    def _read_image(self, img_path, mask_path):
+        '''
+        Read an image and its corresponding mask. And check if they are valid.
+        images are assumed to be binary in single channel
+        '''
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR_RGB)
+        if img is None:
+            raise ValueError(f"Image {img_path} could not be read.")
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        if mask is None:
+            raise ValueError(f"Mask {mask_path} could not be read.")
+        if img.shape[:2] != mask.shape[:2]:
+            raise ValueError(f"Image {img_path} and mask {mask_path} dimensions do not match.")
+        if mask.max() > 1:
+            mask = mask // 255  # Normalize mask to 0-1 range
+        mask = np.ceil(mask).astype(np.uint8)
+        return img, mask
+    
+   
+    
+transforms = [
+    pp.Crop(range_crop_len=(200, 1000), n_copy=10),
+    pp.Resize(target_size=(448, 448))
+]
+
+dataset = loadedDataset(
+    dataset_img_path="img_debug",
+    dataset_mask_path="mask_debug",
+    augmentations=transforms
+)
+
+print(f"Dataset length: {len(dataset)}")
+for i in range(len(dataset)):
+    img, mask = dataset[i]
+    print(f"Image {i} shape: {img.shape}, Mask {i} shape: {mask.shape}")
+    
+    # Convert mask to 3-channel for concatenation with image
+    mask_3ch = np.expand_dims(mask, axis=2)  # Add channel dimension
+    mask_3ch = np.repeat(mask_3ch, 3, axis=2)  # Repeat to get 3 channels
+    mask_3ch = mask_3ch * 255  # Scale to 0-255 for visibility
+    
+    # Concatenate image and mask horizontally
+    out = np.concatenate((img,mask_3ch), axis=1)
+    cv2.imshow("Output", out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- data/preprocess_pipeline.py
+++ data/preprocess_pipeline.py
@@ -18,12 +18,12 @@ class Augmentation:
         self.use_raw = use_raw
 
 class PreprocessPipeline:
-    def __init__(self, augmentations: list[Augmentation]=[]):
+    def __init__(self, augmentations:list[Augmentation]=[]):
         self.augmentations = augmentations
         
 
     def __call__(self, imgs:list[np.ndarray], masks:list[np.ndarray]):        
-        if self.augmentations!= []:
+        if self.augmentations != []:
             imgs_out, masks_out = [], []
             for aug in self.augmentations:
                 if aug.use_raw:
@@ -103,13 +103,44 @@ class Crop(Augmentation):
         return img_cropped, mask_cropped
         
         
+class RandomJitter(Augmentation):
+    """
+        Randomly jitters the input images and masks.
+        Args:
+            jitter_strength (float): Strength of the jittering effect.
+            n_copy (int): Number of copies to generate for each input image.
+            use_raw (bool): If True, the output from the augmentation module will be generated based on raw images.
+            prop_apply (float): Probability that the jittering will be applied to each copy.
+    """
+    def __init__(self, jitter_strength:float=0.2, n_copy:int=1, use_raw:bool=False, prop_apply:float=0.5):
+        super().__init__('RandomJitter', n_copy=n_copy, use_raw=use_raw)
+        self.jitter_strength = jitter_strength
+        self.prop_apply = prop_apply
+
+    def __call__(self, imgs:list[np.ndarray], masks:list[np.ndarray]):
+        imgs_out, masks_out = [], []
+        for i in range(len(imgs)):
+            for _ in range(self.n_copy):
+                if random.random() < self.prop_apply:
+                    # Apply jittering
+                    jittered_img = imgs[i] + np.random.uniform(-self.jitter_strength * 255 , self.jitter_strength * 255, imgs[i].shape)
+                    jittered_img = np.clip(jittered_img, 0, 255)
+                    jittered_img = jittered_img.astype(np.uint8)
+                else:
+                    # No jittering
+                    jittered_img = imgs[i]
+                
+                imgs_out.append(jittered_img)
+                masks_out.append(masks[i])
+        return imgs_out, masks_out
+    
 if __name__ == "__main__":
     # Example usage
     img = cv2.imread('img_debug/3.jpg')
     mask = cv2.imread('mask_debug/3_GT.png', cv2.IMREAD_GRAYSCALE)
     
     pipeline = PreprocessPipeline([
-        Crop(range_crop_len=(200,1000), n_copy=10),
+        RandomJitter(use_raw=True, jitter_strength=0.1, n_copy=5),
     ])
     
     imgs_out, masks_out = pipeline([img], [mask])
